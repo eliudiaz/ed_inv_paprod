0031e2d6e40403f565c4afcd2be2c670de371da6
eliu diaz <eliudiaz@gmail.com>
2016-01-19 06:57:26 -0600

    data loading and pointing to production

--- public_html/app/STANDARD/assets/js/controllers/fo_viewdatasetCtrl.js
+++ public_html/app/STANDARD/assets/js/controllers/fo_viewdatasetCtrl.js
@@ -5,9 +5,10 @@
 /**
  * controllers that generate the list of icons (Fontawesome and Themify)
  */
-app.controller('fo_viewdatasetCtrl', ["$scope", "eReq","$stateParams", function ($scope, eReq,$stateParams) {
+app.controller('fo_viewdatasetCtrl', ["$scope", "eReq", "$stateParams", function ($scope, eReq, $stateParams) {
         var reqContent = eReq.getInstance("http://181.209.238.78:5000/v1/opendata");
-        var jsonData = reqContent.get("/content/entities/"+$stateParams.serie);
+//        var reqContent = eReq.getInstance("http://localhost:5000/v1/opendata");
+        var jsonData = reqContent.get("/content/entities/" + $stateParams.serie);
         function setPivotReport() {
             var report = {
                 dataSourceType: "json",
@@ -22,7 +23,8 @@ app.controller('fo_viewdatasetCtrl', ["$scope", "eReq","$stateParams", function 
         }
         // Init Flexmonster
         flexmonster.embedPivotComponent("flexmonster/", "pivot-container", "100%", "500", {
-            licenseKey: "Z544-5U1SI3-3D1H-2J22-0U37-4L2A-0M41-3F",
+//            licenseKey: "Z544-5U1SI3-3D1H-2J22-0U37-4L2A-0M41-3F",
+            licenseKey: "Z54C-3E2HS4-381I-2758-1U1P-6T0M-2K3S-3C05-572W-0I5E-2B1A-23",
             jsPivotCreationCompleteHandler: setPivotReport
         });
     }]);
